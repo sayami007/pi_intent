2b2ccf725a025dee8b96217c423e01eb8c7a0c15
Bibesh Manandhar <bibesh.manandhar@gmail.com>
2018-08-07 16:55:36 +0545

    Updted

--- app/src/main/java/com/planetinnovative/pi_intent/MainActivity.java
+++ app/src/main/java/com/planetinnovative/pi_intent/MainActivity.java
@@ -7,9 +7,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
-    EditText etUserName;
-    Button btnSend;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText etUserName;
+    private Button btnSend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +17,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         etUserName = findViewById(R.id.etUsername);
         btnSend = findViewById(R.id.btnSend);
-        btnSend.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), DisplayActivity.class);
-                intent.putExtra(Constant.KEY_USERNAME, etUserName.getText().toString());
-                startActivity(intent);
-            }
-        });
+        btnSend.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(getApplicationContext(), DisplayActivity.class);
+        intent.putExtra(Constant.KEY_USERNAME, etUserName.getText().toString());
+        startActivity(intent);
     }
 }
